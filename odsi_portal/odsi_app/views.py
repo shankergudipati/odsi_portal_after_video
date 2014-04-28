65b7434b2817a5961e50b6d8c7894814dab5b099
root <root@ubuntu.persistent.co.in>
2014-04-28 18:04:19 +0530

    stable after details to popup

---
+++ odsi_portal/odsi_app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+def odsi_portal_homepage(request):
+    return render(request,'homepage.html')
+
+
+
+
+# Create your views here.
---
+++ odsi_portal/odsi_portal/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import patterns, include, url
+from odsi_app.views import *
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'odsi_portal.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+    url(r'^odsiportal/',odsi_portal_homepage),
+    url(r'^admin/', include(admin.site.urls)),
+)
